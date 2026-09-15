ae7b61ecaad04a0cbc28e8edbecf7a391535c1ce
NibbleByte <NibbleByte3@gmail.com>
2026-09-15 23:51:50 +0300

    Adding "Samples~" folder in the package (created with symbolic link to be kept in sync).

---
+++ Assets/LinkSamples.bat
@@ -0,0 +1,4 @@
+:: Use this to create a "Samples~" link so the package has the samples as hidden folder, but you can still author them in the original folder.
+:: You may need to delete the initial "Samples~" folder first.
+mklink /J "DevLocker\Audio\WiseAudioPlayer\Samples~" "Samples"
+pause
